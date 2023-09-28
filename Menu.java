cf55edd33c92dbcd7fe1863b994875b5899ab6d5
IsidoraCis <127423300+IsidoraCis@users.noreply.github.com>
2023-09-28 16:27:34 -0300

    Actualización de menú
    Se agregó el menú de gestión de actividades y el de alumnos.
    
    Co-Authored-By: Rodolfo Fernández <127540914+JKsRay@users.noreply.github.com>
    Co-Authored-By: Nikolai Navea <127423342+NikoNavea@users.noreply.github.com>

--- Menu.java
+++ Menu.java
@@ -69,4 +69,383 @@ public class Menu {
         frame.add(panelPrincipal, BorderLayout.CENTER);
         frame.setVisible(true);
     }
+
+    private void mostrarMenuActividades() {
+        JPanel panelActividades = new JPanel(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        gbc.gridy++;
+        gbc.weighty = 1.0;
+        panelPrincipal.add(new JLabel(), gbc);
+
+        JButton btnAgregar = new JButton("Agregar Actividad");
+        JButton btnEliminar = new JButton("Eliminar Actividad");
+        JButton btnMostrar = new JButton("Mostrar Actividades");
+        JButton btnCambiar = new JButton("Cambiar el encargado de una actividad");
+        JButton btnInfo = new JButton("Información sobre una actividad");
+        JButton btnEncargado = new JButton("Información de un encargado");
+        JButton btnVolver = new JButton("Volver al menú principal");
+        
+        //Llamado a metodo agregarActividad
+        btnAgregar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JPanel panelDatosActividad = new JPanel();
+                panelDatosActividad.setLayout(new BoxLayout(panelDatosActividad, BoxLayout.Y_AXIS));
+
+                JTextField nombreField = new JTextField(20);
+                JTextField claveHorariaField = new JTextField(20);
+                JTextField diaField = new JTextField(20);
+
+                panelDatosActividad.add(new JLabel("Nombre de la actividad:"));
+                panelDatosActividad.add(nombreField);
+
+                panelDatosActividad.add(new JLabel("Clave Horaria:"));
+                panelDatosActividad.add(claveHorariaField);
+
+                panelDatosActividad.add(new JLabel("Día:"));
+                panelDatosActividad.add(diaField);
+
+                int option = JOptionPane.showOptionDialog(
+                        null,
+                        panelDatosActividad,
+                        "Ingrese los siguientes datos de la actividad:",
+                        JOptionPane.OK_CANCEL_OPTION,
+                        JOptionPane.PLAIN_MESSAGE,
+                        null,
+                        new Object[]{"OK", "Cancel"},
+                        "OK"
+                );
+
+
+                // Verificar si el usuario hizo clic en OK
+                if (option == JOptionPane.OK_OPTION) {
+
+                    JPanel panelDatosEncargado = new JPanel();
+                    
+                    panelDatosEncargado.setLayout(new BoxLayout(panelDatosEncargado, BoxLayout.Y_AXIS));
+
+                    JTextField nombreEField = new JTextField(20);
+                    JTextField apellidoEField = new JTextField(20);
+                    JTextField rutEField = new JTextField(20);
+                    JTextField cargoEField = new JTextField(20);
+                    JTextField telefonoEField = new JTextField(20);
+                    JTextField correoEField = new JTextField(20);
+
+                    panelDatosEncargado.add(new JLabel("Nombre del encargado:"));
+                    panelDatosEncargado.add(nombreEField);
+                    
+                    panelDatosEncargado.add(new JLabel("Apellido del encargado:"));
+                    panelDatosEncargado.add(apellidoEField);
+
+                    panelDatosEncargado.add(new JLabel("Rut del encargado:"));
+                    panelDatosEncargado.add(rutEField);
+
+                    panelDatosEncargado.add(new JLabel("Cargo del encargado:"));
+                    panelDatosEncargado.add(cargoEField);
+    
+                    panelDatosEncargado.add(new JLabel("Telefono del encargado:"));
+                    panelDatosEncargado.add(telefonoEField);
+
+                    panelDatosEncargado.add(new JLabel("Correo del encargado:"));
+                    panelDatosEncargado.add(correoEField);
+                    
+                    int option2 = JOptionPane.showOptionDialog(
+                        null,
+                        panelDatosEncargado,
+                        "Ingrese los siguientes datos del encargado:",
+                        JOptionPane.OK_CANCEL_OPTION,
+                        JOptionPane.PLAIN_MESSAGE,
+                        null,
+                        new Object[]{"OK", "Cancel"},
+                        "OK"
+                    );
+
+                    if(option2 == JOptionPane.OK_OPTION){
+                        String nombre = nombreEField.getText();
+                        String apellido = apellidoEField.getText();
+                        String rut = rutEField.getText();
+                        String cargo = cargoEField.getText();
+                        String telefono = telefonoEField.getText();
+                        String correo = correoEField.getText();
+
+                        Encargado encargado = new Encargado(nombre, apellido, rut, cargo, telefono, correo);
+
+                        String nombreAct = nombreField.getText();
+                        String claveHoraria = claveHorariaField.getText();
+                        String dia = diaField.getText();
+
+                        coleccion.agregarActividad(new Actividad(nombreAct,dia, claveHoraria, encargado));
+                    }
+                }
+            }
+        });
+
+        //Llamado a metodo eliminarActividad
+        btnEliminar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String actividadEliminar = JOptionPane.showInputDialog("Ingrese el nombre de la actividad que desea eliminar");
+                coleccion.eliminarActividad(actividadEliminar);
+            }
+        });
+
+        //Llamado a metodo mostrarActividades, donde se mostraran todas las actividades
+        btnMostrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                coleccion.mostrarActividades();
+            }
+        });
+        
+        //Llamado a metodo cambiarEncargado
+        btnCambiar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JPanel panelCambiarEncargado = new JPanel();
+                panelCambiarEncargado.setLayout(new BoxLayout(panelCambiarEncargado, BoxLayout.Y_AXIS));
+
+                JTextField actividadEncargadoField = new JTextField(20);
+                JTextField nombreEncargadoField = new JTextField(20);
+                JTextField apellidoEncargadoField = new JTextField(20);
+                JTextField rutEncargadoField = new JTextField(20);
+                JTextField cargoEncargadoField = new JTextField(20);
+                JTextField telefonoEncargadoField = new JTextField(20);
+                JTextField correoEncargadoField = new JTextField(20);
+                
+                
+                panelCambiarEncargado.add(new JLabel("Nombre de la actividad:"));
+                panelCambiarEncargado.add(actividadEncargadoField);
+                
+                panelCambiarEncargado.add(new JLabel("Nombre del nuevo encargado:"));
+                panelCambiarEncargado.add(nombreEncargadoField);
+
+                panelCambiarEncargado.add(new JLabel("Apellido del encargado:"));
+                panelCambiarEncargado.add(apellidoEncargadoField);
+
+                panelCambiarEncargado.add(new JLabel("Rut del encargado:"));
+                panelCambiarEncargado.add(rutEncargadoField);
+
+                panelCambiarEncargado.add(new JLabel("Cargo del encargado:"));
+                panelCambiarEncargado.add(cargoEncargadoField);
+
+                panelCambiarEncargado.add(new JLabel("Telefono del encargado:"));
+                panelCambiarEncargado.add(telefonoEncargadoField);
+
+                panelCambiarEncargado.add(new JLabel("Correo del encargado:"));
+                panelCambiarEncargado.add(correoEncargadoField);
+
+                //Encargado encargado = new Encargado(nombre, apellido, rut, cargo, telefono, correo)
+
+                int option = JOptionPane.showOptionDialog(
+                    null,
+                    panelCambiarEncargado,
+                    "Cambiar el encargado de una actividad:",
+                    JOptionPane.OK_CANCEL_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    new Object[]{"Cambiar", "Cancelar"},
+                    "Cambiar"
+                );
+
+                if (option == JOptionPane.OK_OPTION) {
+                    
+                    String actividadEncargado = actividadEncargadoField.getText();
+                    Encargado nuevoEncargado = new Encargado(nombreEncargadoField.getText(), 
+                    apellidoEncargadoField.getText(), rutEncargadoField.getText(), cargoEncargadoField.getText(), 
+                    telefonoEncargadoField.getText(), correoEncargadoField.getText());
+
+                    coleccion.cambiarEncargado(nuevoEncargado, actividadEncargado);
+                }
+            }
+        });
+
+        //Llamado a metodo mostrarActividades, donde se mostrara informacion de una actividad en especifico
+        btnInfo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String nombreActividad = JOptionPane.showInputDialog("Ingrese el nombre de la actividad que desea ver");
+                coleccion.mostrarActividades(nombreActividad);
+            }
+        });
+
+        //Llamado a metodo mostrarInfoEncargado.
+        btnEncargado.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String rut = JOptionPane.showInputDialog("Ingrese el rut del encargado que desea ver");
+                coleccion.mostrarInfoEncargado(rut);
+            }
+        });
+
+        //Accion de volver al menu principal
+        btnVolver.addActionListener(new ActionListener() {
+         
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        btnVolver.addActionListener(new ActionListener() {
+        
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(panelActividades);
+                frame.add(panelPrincipal);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+
+        gbc.gridx = 0;
+        gbc.gridwidth = 2;
+        panelActividades.add(btnAgregar, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnEliminar, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnMostrar, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnCambiar, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnInfo, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnEncargado, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnVolver, gbc);
+
+        frame.remove(panelPrincipal);
+        frame.add(panelActividades);
+        frame.revalidate();
+        frame.repaint();
+    }
+
+    private void mostrarMenuAlumnos() {
+        JPanel panelActividades = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        gbc.gridy++;
+        gbc.weighty = 1.0;
+        panelPrincipal.add(new JLabel(), gbc);
+
+        JButton btnAgregar = new JButton("Agregar alumno");
+        JButton btnEliminar = new JButton("Eliminar alumno de una actividad");
+        JButton btnMostrar = new JButton("Mostrar todos los alumnos de la Universidad");
+        JButton btnMostrarInfoAlumno = new JButton("Mostrar datos de un alumno");
+        JButton btnVolver = new JButton("Volver al menú principal");
+        
+        //Llamado a metodo agregarAlumno
+        btnAgregar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String nombreAct = JOptionPane.showInputDialog("Ingrese el nombre de la actividad a la que desea inscribir al alumno: ");
+                if(coleccion.verifActividad(nombreAct) == false){
+                    return; 
+                }
+                String nombre = JOptionPane.showInputDialog("Ingrese el nombre del alumno: ");
+                String apellido = JOptionPane.showInputDialog("Ingrese el apellido del alumno: ");
+                String rut = JOptionPane.showInputDialog("Ingrese el rut del alumno: ");
+                String carrera = JOptionPane.showInputDialog("Ingrese la carrera del alumno: ");
+                String anioIngreso = JOptionPane.showInputDialog("Ingrese el año de ingreso del alumno: ");
+
+                Actividad actividad = coleccion.getMapaActividades().get(nombreAct);
+                Alumnos alumno = new Alumnos(nombre, apellido, rut, carrera, anioIngreso);
+                
+                if(actividad.agregarAlumno(alumno, actividad)){
+                    coleccion.agregarAlumnoListaMaestra(alumno);
+                }
+
+                JOptionPane.showMessageDialog(null, "Acción realizada con éxito");
+            }
+        });
+
+        //Llamado a metodo eliminarAlumno
+        btnEliminar.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                String opcionString;
+                String nombreAct2;
+                
+                do{
+                    opcionString = JOptionPane.showInputDialog("Ingrese si quiere eliminar al alumno por rut o nombre: ");
+                    opcionString = opcionString.toLowerCase();
+                }while(!opcionString.equals("rut") == false && !opcionString.equals("nombre") == false);
+                            
+                nombreAct2 = JOptionPane.showInputDialog("Ingrese el nombre de la actividad de la que desea eliminar al alumno: ");
+
+                if(!coleccion.verifActividad(nombreAct2)){
+                    return; 
+                }
+
+                Actividad actividadEliminacion = coleccion.getMapaActividades().get(nombreAct2);
+           
+                if(opcionString.equals("rut")){
+                    String rutEliminar = JOptionPane.showInputDialog("Ingrese el rut del alumno que desea eliminar: ");
+                    if(actividadEliminacion.eliminarAlumno(actividadEliminacion, rutEliminar)){
+                        coleccion.eliminarAlumnoListaMaestra(rutEliminar);
+                    }
+                } else{
+                    String nombreEliminar = JOptionPane.showInputDialog("Ingrese el nombre del alumno que desea eliminar: ");
+                                
+                    String apellidoEliminar = JOptionPane.showInputDialog("Ingrese el apellido del alumno que desea eliminar: ");
+                    if(actividadEliminacion.eliminarAlumno(actividadEliminacion, nombreEliminar, apellidoEliminar)){
+                        coleccion.eliminarAlumnoListaMaestra(nombreEliminar, apellidoEliminar);
+                    }
+                    actividadEliminacion.eliminarAlumno(actividadEliminacion, nombreEliminar, apellidoEliminar);
+                }
+
+            }
+        });
+
+        
+        //Llamado a metodo mostrarAlumnosListaMaestra
+        btnMostrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                coleccion.mostrarAlumnosListaMaestra();
+            }
+        });
+
+        
+        //Llamado a metodo mostrarInfoAlumno
+        btnMostrarInfoAlumno.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String rut = JOptionPane.showInputDialog("Ingrese el rut del alumno que desea ver su información: "); 
+                coleccion.mostrarInfoAlumno(rut);
+            }
+        });
+
+
+        //Accion de volver al menu principal
+        btnVolver.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(panelActividades);
+                frame.add(panelPrincipal);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+
+        gbc.gridx = 0;
+        gbc.gridwidth = 2;
+        panelActividades.add(btnAgregar, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnEliminar, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnMostrar, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnMostrarInfoAlumno, gbc);
+
+        gbc.gridy++;
+        panelActividades.add(btnVolver, gbc);
+
+        frame.remove(panelPrincipal);
+        frame.add(panelActividades);
+        frame.revalidate();
+        frame.repaint();
+    }
 }
