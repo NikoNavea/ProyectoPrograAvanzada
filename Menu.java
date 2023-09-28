316385647128fea2926e6859f5510ea05f51777b
Nikolai Navea <nikolai.lukas@gmail.com>
2023-09-28 15:54:29 -0300

    Inicialización de ventanas en la colección Menu

--- Menu.java
+++ Menu.java
@@ -1,34 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Menu {
-    public void mostrarMenu(){
-        System.out.println("");
-        System.out.println("Menú Principal");
-        System.out.println("----------------------------------");
-        System.out.println("1. Gestión de Actividades");
-        System.out.println("2. Gestión de Alumnos");
-        System.out.println("0. Salir");
-        System.out.print("Seleccione una opción: ");
-    }
-    public void mostrarMenuActividades(){
-        System.out.println("Menú de Gestión de Actividades");
-        System.out.println("----------------------------------");
-        System.out.println("1. Agregar actividad");
-        System.out.println("2. Eliminar actividad");
-        System.out.println("3. Mostrar actividades");
-        System.out.println("4. Cambiar el encargado de una actividad");
-        System.out.println("5. Información sobre una actividad");
-        System.out.println("0. Volver al menú principal");
-        System.out.print("Seleccione una opción: ");
-        System.out.println("");
-    }   
+    private JFrame frame;
+    private JPanel panelPrincipal;
+    private ColeccionActividades coleccion;
+
     
-    public void mostrarMenuAlumnos(){
-        System.out.println("Menú de Gestión de Alumnos");
-        System.out.println("----------------------------------");
-        System.out.println("1. Agregar alumno");
-        System.out.println("2. Eliminar alumno de una actividad");
-        System.out.println("3. Mostrar alumnos");
-        System.out.println("0. Volver al menú principal");
-        System.out.print("Seleccione una opción: ");
-        System.out.println("");
+    public Menu() {
+        frame = new JFrame("Sistema de Planificación");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLayout(new BorderLayout());
+        coleccion = new ColeccionActividades();
+        mostrarMenuPrincipal();
     }
 }
